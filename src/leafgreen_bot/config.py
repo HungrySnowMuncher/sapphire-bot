9832464eb371ad5d4828b184b8cfa09318e96c4d
Joshua Kouri <kourijoshua@gmail.com>
2026-03-14 00:00:08 -0400

    Added launch method to GBA interface class

--- src/leafgreen_bot/config.py
+++ src/leafgreen_bot/config.py
@@ -12,6 +12,11 @@ def get_required_env(key: str) -> str:
         raise KeyError(f"Missing required environment variable: {key}")
     return value
 
+# mGBA executable file path.
 MGBA_EXECUTABLE_PATH = get_required_env("MGBA_EXECUTABLE_PATH")
 if not os.path.isfile(MGBA_EXECUTABLE_PATH):
     raise FileNotFoundError(f"MGBA executable not found: {MGBA_EXECUTABLE_PATH}")
+# LeafGreen file rom path.
+LEAFGREEN_ROM_PATH = get_required_env("LEAFGREEN_ROM_PATH")
+if not os.path.isfile(LEAFGREEN_ROM_PATH):
+    raise FileNotFoundError(f"Leafgreen rom not found: {LEAFGREEN_ROM_PATH}")
--- src/leafgreen_bot/interface/interface.py
+++ src/leafgreen_bot/interface/interface.py
@@ -1,5 +1,30 @@
+import os
+import subprocess
+
+from leafgreen_bot.config import MGBA_EXECUTABLE_PATH, LEAFGREEN_ROM_PATH
+
+
 class Interface:
     """ Provides an Interface to the GBA emulator. """
 
     def __init__(self):
         """ Constructor. """
+        self._process: subprocess.Popen | None = None
+
+    def launch(self) -> bool:
+        """ Launch the GBA emulator with the LeafGreen rom loaded.
+
+        Returns:
+            (bool): Whether the emulator was successfully launched.
+        """
+        commands = [MGBA_EXECUTABLE_PATH, LEAFGREEN_ROM_PATH]
+        try:
+            self._process = subprocess.Popen(commands)
+            print(f"Launched {os.path.basename(MGBA_EXECUTABLE_PATH)} successfully.")
+            return True
+        except FileNotFoundError:
+            if not os.path.isfile(MGBA_EXECUTABLE_PATH):
+                print(f"Error: Could not find the mGBA executable at \"{MGBA_EXECUTABLE_PATH}\".")
+            if not os.path.isfile(LEAFGREEN_ROM_PATH):
+                print(f"Error: Could not find the LeafGreen rom file at \"{LEAFGREEN_ROM_PATH}\".")
+            return False
